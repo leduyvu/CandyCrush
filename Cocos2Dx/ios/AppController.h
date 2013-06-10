fc060ea7215eb5b1a97406f17eb2442967a9e334
Your Name HereAce <nghiem.tuan.minh@framgia.com>
2013-06-10 12:52:47 +0900

    Initial Commit

---
+++ Cocos2Dx/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  Cocos2DxAppController.h
+//  Cocos2Dx
+//
+//  Created by MinhNT on 13/05/16.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
